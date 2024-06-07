bd9d74a363802167b38d8aa7479c1870b0ade26b
john okello <jkokello68@gmail.com>
2024-06-06 17:43:58 -0700

    basic_python.py

--- basic_python.py
+++ basic_python.py
@@ -3,7 +3,6 @@ height=input("Please enter your height: ")
 weight=input("Please enter your weight:  ")
 
 
-
 # check BMI
 height_as_float=float(height)
 weight_as_float=float(weight)
@@ -11,6 +10,7 @@ weight_as_float=float(weight)
 BMI = weight_as_float / (height_as_float * height_as_float)
 
 print(round(BMI, 2))
+
 
 # How many weeks you have to live 
 age_as_int=int(age)
@@ -22,16 +22,18 @@ print(f"You have approximately {weeks_left} weeks left.")
 # fsting 
 print(f"my name is john, I am {age} years old. ")
 
+
 # Tip claculator 
+
 print("Welcome to the tip calculator")
 
-bill= float(input("What is the total bill? "))
+bill= float(input("What is the total bill? $ "))
 tip=int(input("How much tip would you like to give? 10, 12, or 15? "))
-total_people=input=("How many people are going to spit the bill? ")
+people=int(input("How many people are going to spit the bill? "))
 
+total_Calculated_tip= (bill * float(tip / 100))
+tip_per_person = (float(total_Calculated_tip / people))
+tip_per_person_rounded =round(tip_per_person, 2)
 
-total_Calculated_tip= bill * (tip / 100)
-tip_per_person = total_Calculated_tip / total_people
-
-print(f"The tip each person is going to pay is ${tip_per_person} ")
+print(f"The tip each person is going to pay is ${tip_per_person_rounded} ")
 
