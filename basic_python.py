9393171cc240ccdfbf797e83d4345d2ecb8ba0ae
john okello <jkokello68@gmail.com>
2024-06-06 17:05:25 -0700

    basic_python.py

--- basic_python.py
+++ basic_python.py
@@ -3,6 +3,7 @@ height=input("Please enter your height: ")
 weight=input("Please enter your weight:  ")
 
 
+
 # check BMI
 height_as_float=float(height)
 weight_as_float=float(weight)
@@ -15,8 +16,22 @@ print(round(BMI, 2))
 age_as_int=int(age)
 years_left= (90-age_as_int)
 weeks_left = (years_left * 52)
-print(f"You have {weeks_left} weeks left to live according to staticstics pf expected lifespan.")
+print(f"You have approximately {weeks_left} weeks left.")
 
 
 # fsting 
 print(f"my name is john, I am {age} years old. ")
+
+# Tip claculator 
+print("Welcome to the tip calculator")
+
+bill= float(input("What is the total bill? "))
+tip=int(input("How much tip would you like to give? 10, 12, or 15? "))
+total_people=input=("How many people are going to spit the bill? ")
+
+
+total_Calculated_tip= bill * (tip / 100)
+tip_per_person = total_Calculated_tip / total_people
+
+print(f"The tip each person is going to pay is ${tip_per_person} ")
+
