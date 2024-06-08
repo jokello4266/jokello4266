3d665ccfacaa7330a361ec03b9b2dae0886e65f6
john okello <jkokello68@gmail.com>
2024-06-07 19:00:34 -0700

    statements.py

--- statements.py
+++ statements.py
@@ -1,3 +1,6 @@
+
+# if statement 
+
 number=int(input("Enter a number: "))
 print(number)
 
@@ -5,3 +8,21 @@ if number % 2 ==  0:
     print("The number is an even number. ")
 else:
     print("The number is an odd number. ")
+
+
+# nested statements 
+
+print("Welccome to the rollercoaster! ")
+height=input("Enter your height: ")
+age=input("what is your age? ")
+
+if height >= 120:
+    print("You ride the rollercoaster")
+elif age < 12:
+    print("Please pay $5, ")
+elif age <= 18:
+    print("Please pay $ 7. ")
+elif age > 18:
+    print("Please print $ 12.")
+else:
+    print("Sorry you cannot ride the rollercoaster")
